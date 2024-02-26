755c66e31a3c9004c969241a40da6b457bd0b4f7
Mohamed El Mhamdi <mohamed.elmhamdi004@gmail.com>
2024-02-26 15:43:04 +0100

    Agregar archivos de proyecto.

---
+++ Activity2CodeFirst/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Activity2CodeFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
